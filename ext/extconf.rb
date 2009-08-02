b5752020f43bf88c0b807ee8fae408388194fb57
Pavel Valodzka <pavel@valodzka.name>
2009-08-03 01:03:38 +0300

    ad tz database

--- ext/extconf.rb
+++
@@ -1,8 +0,0 @@
-# $: << '/home/pavel/dev/rice/ruby/lib'
-require 'mkmf-rice'
-
-with_config("release") do
-  $defs.push("-DNDEBUG")
-  $defs.push("-DBOOST_DISABLE_ASSERTS")
-end
-create_makefile('fastz')
